75dd504af157444c41d1672c9e857dd79bc0bc4a
gtrunsec <gtrun@outlook.com>
2018-11-30 00:18:33 -0800

    Adjust path

--- NSM/ELK/check_ELK_service.sh
+++
@@ -1,7 +0,0 @@
-sudo systemctl status --no-pager elasticsearch
-sudo systemctl status --no-pager kibana
-sudo systemctl status --no-pager logstash
-sudo systemctl status --no-pager kafka
-sudo systemctl status --no-pager zookeeper
-sudo systemctl status --no-pager suricat
-
