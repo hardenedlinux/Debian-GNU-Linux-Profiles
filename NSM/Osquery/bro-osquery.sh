ca7a7e3eed65b08ae0047a8b3ca7b69a8ce2b0dc
gtrunsec <gtrun@outlook.com>
2018-12-31 14:00:55 -0800

    fix typo

--- NSM/Osquery/bro-osquery.sh
+++ NSM/Osquery/bro-osquery.sh
@@ -16,9 +16,11 @@ sudo gem install fpm
 git clone --recursive https://github.com/iBigQ/osquery
 cd osquery
 make deps
+./tools/provision.sh install osquery/osquery-local/caf
+./tools/provision.sh install osquery/osquery-local/broker
 SKIP_BRO=False make && sudo make install
 
-sudo cp osqueryd.service /etc/systemd/system/
+sudo cp ~/src/Debian-GNU-Linux-Profiles/NSM/Osquery/osqueryd.service /etc/systemd/system/
 
 sudo mkdir /var/osquery/
 sudo mkdir /etc/osquery/
@@ -30,10 +32,11 @@ sudo sed -i -e '/"bro_ip":/s/.*/"bro_ip": "'"${bro_ip}"'",/' /etc/osquery/osquer
 ##load Bro-osquery script to framworkds [[https://www.bro.org/sphinx/quickstart/index.html#telling-bro-which-scripts-to-load][Quick Start Guide — Bro 2.6 documentation]]
 
 sudo sh -c 'echo "@load packages" >> /usr/local/bro/share/bro/site/local.bro'
-sudo broctl deploy
 
 ##check whereis osqueryd, adding osqueryd service .
 sudo systemctl daemon-reload
 sudo systemctl enable osqueryd
 sudo systemctl start osqueryd
+sudo broctl deploy
+
 echo "if you can't loaded serice, plz check osqueryd dir-path. then modify sudo emacs /etc/init.d/osqueryd find in if [ -z $EXEC ]; then EXEC=/usr/local/bin/osqueryd; "
